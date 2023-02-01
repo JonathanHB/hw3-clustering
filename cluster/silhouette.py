960f5ab1c5e3ae05eff44c2488998f8622b90416
jonathanhb <jonathan3141@gmail.com>
2023-01-31 22:47:37 -0800

    silhouette scoring working but with squared distances instead of distances

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -24,8 +24,8 @@ class Silhouette:
         #construct distance matrix (very inefficiently)
         distances = np.ndarray([n,n])
         for i, xi in enumerate(X):
-            for j, xj in enumerate(X[i:]):
-                distances[i][j+i] = np.dot(xi-xj, xi-xj)
+            for j, xj in enumerate(X):
+                distances[i][j] = np.dot(xi-xj, xi-xj)
 
         #calculate silhouette scores
         sscores = []
@@ -34,23 +34,23 @@ class Silhouette:
             #intra-cluster
 
             #collect distances across the ith row
-            intradists1 = [j for x, j in enumerate(distances[i][i+1:]) if y[x+i+1] == y[i]]
-            #collect distances down the ith column
-            if i == 0:
-                intradists2 = []
-            elif i == 1:
-                if y[0] == y[i]:
-                    intradists2 = [distances[0][i]]
-                else:
-                    intradists2 = []
-            else:
-                #print(distances[0:i,i])
-                intradists2 = [j for x, j in enumerate(distances[0:i,i]) if y[x] == y[i]]
-
-            intradists = intradists1+intradists2
+            intradists = [j for x, j in enumerate(distances[i]) if (y[x] == y[i] and x != i)]
+            # #collect distances down the ith column
+            # if i == 0:
+            #     intradists2 = []
+            # elif i == 1:
+            #     if y[0] == y[i]:
+            #         intradists2 = [distances[0][i]]
+            #     else:
+            #         intradists2 = []
+            # else:
+            #     #print(distances[0:i,i])
+            #     intradists2 = [j for x, j in enumerate(distances[0:i,i]) if y[x] == y[i]]
+            #
+            # intradists = intradists1+intradists2
 
             #print(len(intradists1))
-            #print(len(intradists))
+            print(len(intradists))
             #print(n)
             #assert len(intradists) == n/5-1, "wrong intradists"
             cluster_size = len(intradists)
@@ -62,24 +62,24 @@ class Silhouette:
             #inter-cluster
 
             #collect distances across the ith row
-            interdists1 = [j for x, j in enumerate(distances[i][i+1:]) if y[x+i+1] != y[i]]
+            interdists = [j for x, j in enumerate(distances[i]) if y[x] != y[i]]
             #collect distances down the ith column
-            if i == 0:
-                interdists2 = []
-            elif i == 1:
-                if y[0] != y[i]:
-                    interdists2 = [distances[0][i]]
-                else:
-                    interdists2 = []
-            else:
-                interdists2 = [j for x, j in enumerate(distances[0:i,i]) if y[x] != y[i]]
+            # if i == 0:
+            #     interdists2 = []
+            # elif i == 1:
+            #     if y[0] != y[i]:
+            #         interdists2 = [distances[0][i]]
+            #     else:
+            #         interdists2 = []
+            # else:
+            #     interdists2 = [j for x, j in enumerate(distances[0:i,i]) if y[x] != y[i]]
 
             # if i != 0:
             #     interdists2 = [j for x, j in enumerate(distances[0:i][i]) if y[x] != y[i]]
             # else:
             #     interdists2 = []
 
-            interdists = interdists1+interdists2
+            #interdists = interdists1+interdists2
 
             #print(len(interdists))
             #assert len(interdists) == 4*n/5, "wrong interdists"
@@ -127,9 +127,6 @@ def _clustering_test_plot(k, seed, kmseed):
     cl_in = utils.make_clusters(k=k, seed = seed)
     km = kmeans.KMeans(k, .0005, 90)
 
-    sil = Silhouette()
-    silscore = sil.score(np.array(cl_in[0]), np.array(cl_in[1]))
-
     km.fit(cl_in[0], kmseed)
 
     centroids = km.get_centroids()
@@ -137,6 +134,8 @@ def _clustering_test_plot(k, seed, kmseed):
 
     predlabels = km.predict(cl_in[0])
 
+    sil = Silhouette()
+    silscore = sil.score(np.array(cl_in[0]), np.array(predlabels))
 
     plotpoints = cl_in[0]
     truelanels_c = cl_in[1]
